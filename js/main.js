3a070f19598d0abaf868df6a57d312d191c3732b
ApplePi33 <46863207+ApplePi33@users.noreply.github.com>
2022-06-17 13:42:35 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /* Loads Head Info */
 document.getElementById("headInfo").innerHTML =
 "<title>Daydreamer</title>" +
-"<link rel='shortcut icon' href='/images/Logo.png' />" +
-"<link rel='stylesheet' href='/css/main.css' />" +
+"<link rel='shortcut icon' href='/Daydreamer/images/Logo.png' />" +
+"<link rel='stylesheet' href='/Daydreamer/css/main.css' />" +
 "<link rel='preconnect' href='https://fonts.googleapis.com' />" +
 "<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />" +
 "<link href='https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;600&display=swap' rel='stylesheet'/>";
@@ -10,18 +10,18 @@ document.getElementById("headInfo").innerHTML =
 /* Loads Navigation Bar */
 document.getElementById("navBar").innerHTML =
   "<nav class='navbar'>" +
-  "<div class='navbar__container'><a href='/index.html'><img src='/images/Logo.png' class='navbar__logo'/></a>" +
+  "<div class='navbar__container'><a href='/Daydreamer/index.html'><img src='/Daydreamer/Logo.png' class='navbar__logo'/></a>" +
   "<div class='navbar__toggle' id='mobile-menu'>" +
   "<span class='bar'></span>" +
   "<span class='bar'></span>" +
   "<span class='bar'></span>" +
   "</div>" +
   "<ul class='navbar__menu'>" +
-  "<li class='navbar__item'><a href='/categories/music.html' class='navbar__links'> Music </a></li>" +
-  "<li class='navbar__item'><a href='/categories/twitch.html' class='navbar__links'>Twitch </a></li>" +
-  "<li class='navbar__item'><a href='/categories/youtube.html' class='navbar__links'>YouTube</a></li>" +
-  "<li class='navbar__item'><a href='/categories/submissions.html' class='navbar__links'>Submissions</a></li>" +
-  "<li class='navbar__button'><a href='/categories/Login/login.html' class='button'> Log In </a></li>" +
+  "<li class='navbar__item'><a href='/Daydreamer/categories/music.html' class='navbar__links'> Music </a></li>" +
+  "<li class='navbar__item'><a href='/Daydreamer/twitch.html' class='navbar__links'>Twitch </a></li>" +
+  "<li class='navbar__item'><a href='/Daydreamer/youtube.html' class='navbar__links'>YouTube</a></li>" +
+  "<li class='navbar__item'><a href='/Daydreamer/submissions.html' class='navbar__links'>Submissions</a></li>" +
+  "<li class='navbar__button'><a href='/Daydreamer/Login/login.html' class='button'> Log In </a></li>" +
   "</ul>" +
   "</div>" +
   "</nav>";
