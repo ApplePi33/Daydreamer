eaa6cc7b239df41aa285c0f1476ed47445bc869c
ApplePi33 <46863207+ApplePi33@users.noreply.github.com>
2022-06-23 14:14:05 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* Loads Head Info */
 document.getElementById("headInfo").innerHTML =
-"<title>Daydreamer Music</title>" +
-"<link rel='shortcut icon' href='/Daydreamer/images/Logo.png' />" +
-"<link rel='stylesheet' href='/Daydreamer/css/main.css' />" +
-"<link rel='preconnect' href='https://fonts.googleapis.com' />" +
-"<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />" +
-"<link href='https://fonts.googleapis.com/css2?family=Viga&display=swap' rel='stylesheet'></link>" +
-"<link href='https://fonts.googleapis.com/css2?family=Mulish:wght@500&display=swap' rel='stylesheet'>";
+  "<title>Daydreamer Music</title>" +
+  "<link rel='shortcut icon' href='/Daydreamer/images/Logo.png' />" +
+  "<link rel='stylesheet' href='/Daydreamer/css/main.css' />" +
+  "<link rel='preconnect' href='https://fonts.googleapis.com' />" +
+  "<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />" +
+  "<link href='https://fonts.googleapis.com/css2?family=Viga&display=swap' rel='stylesheet'></link>" +
+  "<link href='https://fonts.googleapis.com/css2?family=Mulish:wght@500&display=swap' rel='stylesheet'>";
 
 /* Loads Navigation Bar */
 document.getElementById("navBar").innerHTML =
@@ -27,44 +27,44 @@ document.getElementById("navBar").innerHTML =
   "</div>" +
   "</nav>";
 
-  /* Loads Footer */
+/* Loads Footer */
 document.getElementById("footer").innerHTML =
-"<div class='footer__container'>" +
+  "<div class='footer__container'>" +
   "<div class='footer__cards'>" +
-    "<div class='footer__cards--wrapper'>" +
-      "<div class='footer__cards--items'>" +
-        "<h2>About Us</h2>" +
-        "<p>Our goal is to help small artists share their music with the world, by giving them a platform and connecting them with curators.</p>" +
-        "</div>" +
-        "<div class='footer__cards--items'>" +
-        "<h2>Categories</h2>" +
-        "<a href='/Daydreamer/categories/youtube.html'>Youtube</a>" +
-        "<a href='/Daydreamer/categories/music.html'>Music</a>" +
-        "<a href='/Daydreamer/categories/submissions.html'>Submissions</a>" +
-        "<a href='/Daydreamer/categories/twitch.html'>Twitch</a>" +
-        "</div>" +
-        "</div>" +
-        "<div class='footer__cards--wrapper'>" +
-        "<div class='footer__cards--items'>" +
-        "<h2>Socials</h2>" +
-        "<a href='https://discord.gg/6bb24kFMas' target='_blank'>Discord</a>" +
-        "<a href='https://www.submithub.com/blog/daydreamer-music' target='_blank'>Submit Hub</a>" +
-        "<a href='https://www.youtube.com/DaydreamerMusicOfficial' target='_blank'>YouTube</a>" +
-        "<a href='https://www.twitch.tv/applepi33' target='_blank'>Twitch</a>" +
-        "</div>" +
-        "</div>" +
-        "</div>" +
-        "<div class='footer__copyright'>" +
-        "<p>&copy; 2022 Daydreamer Music All rights reserved" +
-        "</p>" +
-        "</div>" +
-        "</div>";
+  "<div class='footer__cards--wrapper'>" +
+  "<div class='footer__cards--items'>" +
+  "<h2>About Us</h2>" +
+  "<p>Our goal is to help small artists share their music with the world, by giving them a platform and connecting them with curators.</p>" +
+  "</div>" +
+  "<div class='footer__cards--items'>" +
+  "<h2>Categories</h2>" +
+  "<a href='/Daydreamer/categories/youtube.html'>Youtube</a>" +
+  "<a href='/Daydreamer/categories/music.html'>Music</a>" +
+  "<a href='/Daydreamer/categories/submissions.html'>Submissions</a>" +
+  "<a href='/Daydreamer/categories/twitch.html'>Twitch</a>" +
+  "</div>" +
+  "</div>" +
+  "<div class='footer__cards--wrapper'>" +
+  "<div class='footer__cards--items'>" +
+  "<h2>Socials</h2>" +
+  "<a href='https://discord.gg/6bb24kFMas' target='_blank'>Discord</a>" +
+  "<a href='https://www.submithub.com/blog/daydreamer-music' target='_blank'>Submit Hub</a>" +
+  "<a href='https://www.youtube.com/DaydreamerMusicOfficial' target='_blank'>YouTube</a>" +
+  "<a href='https://www.twitch.tv/applepi33' target='_blank'>Twitch</a>" +
+  "</div>" +
+  "</div>" +
+  "</div>" +
+  "<div class='footer__copyright'>" +
+  "<p>&copy; 2022 Daydreamer Music All rights reserved" +
+  "</p>" +
+  "</div>" +
+  "</div>";
 
 /* Mobile Menu */
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+menu.addEventListener('click', function () {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
 })
