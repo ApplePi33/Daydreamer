f920dd0c1dbe79ffcd6cf221ef62d637f904b077
ApplePi33 <46863207+ApplePi33@users.noreply.github.com>
2022-06-26 16:59:24 +0200

    init

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ document.getElementById("navBar").innerHTML =
   "</div>" +
   "<ul class='navbar__menu'>" +
   "<li class='navbar__item'><a href='/Daydreamer/categories/blog.html' class='navbar__links'> Blog </a></li>" +
-  "<li class='navbar__item'><a href='/Daydreamer/categories/twitch.html' class='navbar__links'>Twitch </a></li>" +
+  "<li class='navbar__item'><a href='/Daydreamer/categories/gallery.html' class='navbar__links'>Gallery </a></li>" +
   "<li class='navbar__item'><a href='/Daydreamer/categories/youtube.html' class='navbar__links'>YouTube</a></li>" +
   "<li class='navbar__button'><a href='/Daydreamer/categories/submissions.html' class='button'> Submit </a></li>" +
   "</ul>" +
@@ -40,7 +40,7 @@ document.getElementById("footer").innerHTML =
   "<a href='/Daydreamer/categories/youtube.html'>Youtube</a>" +
   "<a href='/Daydreamer/categories/blog.html'>Blog</a>" +
   "<a href='/Daydreamer/categories/submissions.html'>Submissions</a>" +
-  "<a href='/Daydreamer/categories/twitch.html'>Twitch</a>" +
+  "<a href='/Daydreamer/categories/gallery.html'>Gallery</a>" +
   "</div>" +
   "</div>" +
   "<div class='footer__cards--wrapper'>" +
